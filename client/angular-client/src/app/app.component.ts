e380bdbe87b465aa1646d14a0959bddd27972672
Sooruj Ghangass <sghangas@uwo.ca>
2017-12-01 21:08:50 +0000

    Reorganized file structure to accomodate back-end and front-end servers

---
+++ client/angular-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "Sooruj's SE 3316 Lab 5 NASA Thing";
+}
